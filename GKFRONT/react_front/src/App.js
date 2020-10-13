4525a30790697ae77052b92da86723198a5443f8
Joel J. De Alba <sightlinepcs@outlook.com>
2020-10-13 03:00:47 -0400

    GK_FILE_UPDATE_001

---
+++ GKFRONT/react_front/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+        <header className="GK-CH01">
+          <div className="GK-CL01"></div>
+          <div className="GK-CN01">
+            <ul>
+              <li>Link1</li>
+              <li>Link2</li>
+              <li>Link3</li>
+            </ul>
+          </div>
+          <div className="GK-CC01"></div>
+        </header>
+
+        <div className="GK-CT01">
+        </div>
+
+        
+      
+    </div>
+  );
+}
+
+export default App;
+
+// Div Class Names
+// GK-CT01 = main content window
+// GK-CH01 = main content header
+// GK-CL01 = main content logo
+// GK-CN01 = main content navbar
+// GK-CC01 = main content cart
---
+++ GKSERVER/README.md
@@ -0,0 +1,10 @@
+# GKSERVER
+
+Back-End API designed for a more simple approach to IoT, SQL Database, and NAS Server.
+
+API Created and Utilized for Independent Developmental Research
+
+  - Express.js - framework of Node.js and utilized for back end routing.
+  - Nodemon - monitors, updates, and renderes the data of the server.
+  - Handlebar View - for ease of use as html is simplistic.
+
